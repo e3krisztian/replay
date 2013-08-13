c63e62b94a9b4e63975c942d986b4d8c6760daf7
Krisztian Fekete <fkr972@gmail.com>
2013-08-13 15:48:07 +0200

    Context.resolve_plugin_class(plugin_name)

--- replay/context.py
+++ replay/context.py
@@ -1,3 +1,10 @@
+import inspect
+
+import zope.dottedname.resolve as dottedname
+
+from replay import plugins
+
+
 class Context(object):
 
     datastore = None  # External
@@ -25,3 +32,19 @@ class Context(object):
         if plugins:
             with plugins[0]:
                 self.run(plugins[1:])
+
+    def resolve_plugin_class(self, plugin_name):
+        try:
+            plugin_class = dottedname.resolve(plugin_name)
+        except ImportError as e:
+            full_plugin_name = 'replay.plugins.' + plugin_name
+            try:
+                plugin_class = dottedname.resolve(full_plugin_name)
+            except ImportError:
+                raise e
+
+        if not (inspect.isclass(plugin_class)
+                and issubclass(plugin_class, plugins.Plugin)):
+            raise ValueError('{} is not a Plugin'.format(plugin_name))
+
+        return plugin_class
--- replay/tests/test_context.py
+++ replay/tests/test_context.py
@@ -38,3 +38,43 @@ class Test_run(unittest.TestCase):
                 (1, '__exit__')
             ],
             call_trace)
+
+
+XPlugin = plugins.Plugin
+
+
+class Test_resolve_plugin_class(unittest.TestCase):
+
+    @property
+    def context(self):
+        return fixtures.PluginContext().context
+
+    def test_resolve_by_dotted_name(self):
+        plugin_name = 'replay.tests.test_context.XPlugin'
+
+        plugin_class = self.context.resolve_plugin_class(plugin_name)
+        self.assertIs(XPlugin, plugin_class)
+
+    def test_nonexistent_plugin_raises_ImportError(self):
+        plugin_name = 'replay.tests.test_context.NONEXISTENTPlugin'
+
+        with self.assertRaises(ImportError):
+            self.context.resolve_plugin_class(plugin_name)
+
+    def test_non_class_raises_ValueError(self):
+        plugin_name = 'replay.tests.test_context.plugins'
+
+        with self.assertRaises(ValueError):
+            self.context.resolve_plugin_class(plugin_name)
+
+    def test_non_plugin_raises_ValueError(self):
+        plugin_name = '{}.{}'.format(
+            self.__module__,
+            self.__class__.__name__)
+
+        with self.assertRaises(ValueError):
+            self.context.resolve_plugin_class(plugin_name)
+
+    def test_unqualified_plugin_resolved(self):
+        plugin_class = self.context.resolve_plugin_class('Execute')
+        self.assertIs(plugins.Execute, plugin_class)
